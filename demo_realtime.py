2dae313a200006781165827020a45f97b7287369
Akash Sengupta <akash.sengupta1997@gmail.com>
2018-10-25 22:17:57 +0100

    added realtime prediction demo

--- demo_realtime.py
+++ demo_realtime.py
@@ -1,31 +1,12 @@
-"""
-Demo of HMR.
-
-Note that HMR requires the bounding box of the person in the image. The best performance is obtained when max length of the person in the image is roughly 150px.
-
-When only the image path is supplied, it assumes that the image is centered on a person whose length is roughly 150px.
-Alternatively, you can supply output of the openpose to figure out the bbox and the right scale factor.
-
-Sample usage:
-
-# On images on a tightly cropped image around the person
-python -m demo --img_path data/im1963.jpg
-python -m demo --img_path data/coco1.png
-
-# On images, with openpose output
-python -m demo --img_path data/random.jpg --json_path data/random_keypoints.json
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import sys
-from os import listdir
-from os.path import join
 from absl import flags
 import numpy as np
+import cv2
 
-import skimage.io as io
 import tensorflow as tf
 
 from src.util import renderer as vis_util
@@ -40,10 +21,9 @@ flags.DEFINE_string(
     'If specified, uses the openpose output to crop the image.')
 
 
-def visualize(img, proc_param, joints, verts, cam, image_path):
+def visualize(img, proc_param, joints, verts, cam, save_path, frame_num):
     """
     Renders the result in original image coordinate frame.
-    Saves result in same directory as image_path.
     """
     cam_for_render, vert_shifted, joints_orig = vis_util.get_original(
         proc_param, verts, cam, joints, img_size=img.shape[:2])
@@ -59,49 +39,52 @@ def visualize(img, proc_param, joints, verts, cam, image_path):
     rend_img_vp2 = renderer.rotated(
         vert_shifted, -60, cam=cam_for_render, img_size=img.shape[:2])
 
-    import matplotlib.pyplot as plt
-    # plt.ion()
-    plt.figure(1)
-    plt.clf()
-    plt.subplot(231)
-    plt.imshow(img)
-    plt.title('input')
-    plt.axis('off')
-    plt.subplot(232)
-    plt.imshow(skel_img)
-    plt.title('joint projection')
-    plt.axis('off')
-    plt.subplot(233)
-    plt.imshow(rend_img_overlay)
-    plt.title('3D Mesh overlay')
-    plt.axis('off')
-    plt.subplot(234)
-    plt.imshow(rend_img)
-    plt.title('3D mesh')
-    plt.axis('off')
-    plt.subplot(235)
-    plt.imshow(rend_img_vp1)
-    plt.title('diff vp')
-    plt.axis('off')
-    plt.subplot(236)
-    plt.imshow(rend_img_vp2)
-    plt.title('diff vp')
-    plt.axis('off')
-    plt.draw()
-    # plt.show()
-    plt.savefig(image_path + "hmr_result.png", format='png')
+    combined_img = np.concatenate((rend_img_overlay, rend_img_vp1, rend_img_vp2), axis=0)
+    cv2.imshow('frame', combined_img)
+
+    if save_path is not None:
+        cv2.imwrite(save_path + str(frame_num) + "hmr_result.png", combined_img)
+    # import matplotlib.pyplot as plt
+    # # plt.ion()
+    # plt.figure(1)
+    # plt.clf()
+    # plt.subplot(231)
+    # plt.imshow(img)
+    # plt.title('input')
+    # plt.axis('off')
+    # plt.subplot(232)
+    # plt.imshow(skel_img)
+    # plt.title('joint projection')
+    # plt.axis('off')
+    # plt.subplot(233)
+    # plt.imshow(rend_img_overlay)
+    # plt.title('3D Mesh overlay')
+    # plt.axis('off')
+    # plt.subplot(234)
+    # plt.imshow(rend_img)
+    # plt.title('3D mesh')
+    # plt.axis('off')
+    # plt.subplot(235)
+    # plt.imshow(rend_img_vp1)
+    # plt.title('diff vp')
+    # plt.axis('off')
+    # plt.subplot(236)
+    # plt.imshow(rend_img_vp2)
+    # plt.title('diff vp')
+    # plt.axis('off')
+    # plt.draw()
+    # plt.savefig(save_path + str(frame_num) + "hmr_result.png", format='png')
     # import ipdb
     # ipdb.set_trace()
 
 
-def preprocess_image(img_path, json_path=None):
-    img = io.imread(img_path)
+def preprocess_image(img, json_path=None):
     if img.shape[2] == 4:
         img = img[:, :, :3]
 
     if json_path is None:
         if np.max(img.shape[:2]) != config.img_size:
-            print('Resizing so the max image size is %d..' % config.img_size)
+            # print('Resizing so the max image size is %d..' % config.img_size)
             scale = (float(config.img_size) / np.max(img.shape[:2]))
         else:
             scale = 1.
@@ -120,23 +103,33 @@ def preprocess_image(img_path, json_path=None):
     return crop, proc_param, img
 
 
-def main(folder_path, json_path=None):
+def main(save_path, json_path=None):
     sess = tf.Session()
     model = RunModel(config, sess=sess)
+    cap = cv2.VideoCapture(0)
 
-    images = [f for f in listdir(folder_path) if f.endswith('.png') or f.endswith('.jpg')]
-    print('Predicting on all png or jpg images in folder.')
-    for image in images:
-        print('Image:', image)
-        img_path = join(folder_path, image)
-        input_img, proc_param, img = preprocess_image(img_path, json_path)
+    frame_num = 0
+    while (True):
+        # Capture frame-by-frame
+        ret, img = cap.read()
+
+        # Operations on frame
+        input_img, proc_param, img = preprocess_image(img, json_path)
         # Add batch dimension: 1 x D x D x 3
         input_img = np.expand_dims(input_img, 0)
 
         joints, verts, cams, joints3d, theta = model.predict(
             input_img, get_theta=True)
 
-        visualize(img, proc_param, joints[0], verts[0], cams[0], img_path)
+        visualize(img, proc_param, joints[0], verts[0], cams[0], save_path, frame_num)
+
+        frame_num += 1
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
@@ -149,4 +142,5 @@ if __name__ == '__main__':
 
     renderer = vis_util.SMPLRenderer(face_path=config.smpl_face_path)
 
-    main(config.img_path, config.json_path)
+    # save_path = config.img_path
+    main(config.save_path, config.json_path)
