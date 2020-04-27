9a2c752e96af53ec44dfe341e91488e75c489c3c
Akash Sengupta <akash.sengupta1997@gmail.com>
2020-04-27 17:39:19 +0100

    movi evaluation

--- evaluate_movi_shape.py
+++ evaluate_movi_shape.py
@@ -44,62 +44,32 @@ def preprocess_image(img_path, json_path=None):
     return crop, proc_param, img
 
 
-def convert_bbox_centre_hw_to_corners(centre, height, width):
-    x1 = centre[0] - height/2.0
-    x2 = centre[0] + height/2.0
-    y1 = centre[1] - width/2.0
-    y2 = centre[1] + width/2.0
-
-    return np.array([x1, y1, x2, y2])
-
-
-def main(dataset_path, paired=True, path_correction=False):
+def main(movi_dir_path):
     """
-    This function isn't really doing evaluation on H3.6M - it just runs HMR on each H3.6M frame and stores the output.
+    This function isn't really doing evaluation on 3DPW - it just runs HMR on each 3DPW frame and stores the output.
     There is (or will be) a separate script in the pytorch_indirect_learning repo that will do the evaluation and metric
     computations.
     """
     sess = tf.Session()
     model = RunModel(config, sess=sess)
 
-    save_path = '/data/cvfs/as2562/hmr/evaluations/sports_videos_final_dataset'
-    if not paired:
-        save_path += '_unpaired'
+    cropped_frames_dir = os.path.join(movi_dir_path, 'cropped_frames')
+    capture_round = 'f_pg1'
+    save_path = '/data/cvfs/as2562/hmr/evaluations/movi_shape'
     if not os.path.isdir(save_path):
         os.makedirs(save_path)
-    data = np.load(os.path.join(dataset_path, 'sports_videos_eval.npz'))
-    frame_path_per_frame = data['frames_paths']
-    bbox_centres = data['centres']
-    bbox_whs = data['whs']
+    eval_data = np.load(os.path.join(movi_dir_path, 'movi_{}_eval.npz'.format(capture_round)))
+    frame_fnames = eval_data['imgname']
 
+    fname_per_frame = []
     pose_per_frame = []
     shape_per_frame = []
     verts_per_frame = []
     cam_per_frame = []
 
-    for i in tqdm(range(len(frame_path_per_frame))):
-        fpath = frame_path_per_frame[i]
-        if path_correction:
-            fpath = fpath.replace('/scratch2/', '/scratch/')
-        img = io.imread(fpath)
-        if img.shape[2] == 4:
-            img = img[:, :, :3]
-        # Crop inputs to bounding box
-        # Annoyingly, for sports videos dataset, the frames in the cropped_frames dir are not
-        # actually the cropped frames that are input into the network... Need to do further
-        # cropping and resizing.
-        bbox_centre = bbox_centres[i]
-        bbox_wh = bbox_whs[i] * 1.2
-        bbox_corners = convert_bbox_centre_hw_to_corners(bbox_centre, bbox_wh, bbox_wh)
-        top_left = bbox_corners[:2].astype(np.int16)
-        bottom_right = bbox_corners[2:].astype(np.int16)
-        top_left[top_left < 0] = 0
-        bottom_right[bottom_right < 0] = 0
-        img = img[top_left[0]: bottom_right[0], top_left[1]: bottom_right[1]]
-        img = cv2.resize(img, (config.img_size, config.img_size), interpolation=cv2.INTER_LINEAR)
-        input_img = 2 * ((img / 255.) - 0.5)
-        # input_img, proc_param, img = preprocess_image(image_path)
-
+    for frame in tqdm(frame_fnames):
+        image_path = os.path.join(cropped_frames_dir, frame)
+        input_img, proc_param, img = preprocess_image(image_path)
         # Add batch dimension: 1 x D x D x 3
         input_img = np.expand_dims(input_img, 0)
 
@@ -107,12 +77,13 @@ def main(dataset_path, paired=True, path_correction=False):
         poses = thetas[:, 3:3+72]
         shapes = thetas[:, 3+72:]
 
+        fname_per_frame.append(frame)
         pose_per_frame.append(poses)
         shape_per_frame.append(shapes)
         verts_per_frame.append(verts)
         cam_per_frame.append(cams)
 
-    fname_per_frame = np.array(frame_path_per_frame)
+    fname_per_frame = np.array(fname_per_frame)
     np.save(os.path.join(save_path, 'fname_per_frame.npy'), fname_per_frame)
 
     pose_per_frame = np.concatenate(pose_per_frame, axis=0)
@@ -133,14 +104,8 @@ if __name__ == '__main__':
     config(sys.argv)
     # Using pre-trained model, change this to use your own.
     config.load_path = src.config.PRETRAINED_MODEL
-    if src.config.PRETRAINED_MODEL.endswith('model.ckpt-667589'):
-        paired = True
-    elif src.config.PRETRAINED_MODEL.endswith('model.ckpt-99046'):
-        paired = False
-        print('Using unpaired model!')
 
     config.batch_size = 1
-    print('Images from', config.img_path)
+
     renderer = vis_util.SMPLRenderer(face_path=config.smpl_face_path)
-    path_correction = False
-    main(config.img_path, paired=paired, path_correction=path_correction)
+    main(movi_dir_path=config.img_path)
